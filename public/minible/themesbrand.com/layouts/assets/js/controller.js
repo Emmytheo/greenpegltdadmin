f9ed11011207b311f969a1de1acf1975a30efea1
emmytheo <chidi.mgbara@gmail.com>
2021-10-22 16:01:40 +0100

    New Updates

--- public/minible/themesbrand.com/layouts/assets/js/controller.js
+++ public/minible/themesbrand.com/layouts/assets/js/controller.js
@@ -1,15 +1,19 @@
-// const socket = io();
-// // Initialize our Feathers client application through Socket.io
-// // with hooks and authentication.
-// const client = feathers();
-// client.configure(feathers.socketio(socket));
-// // Use localStorage to store our login token
-// client.configure(feathers.authentication());
+
+
+const socket = io();
+// Initialize our Feathers client application through Socket.io
+// with hooks and authentication.
+const client = feathers();
+client.configure(feathers.socketio(socket));
+// Use localStorage to store our login token
+client.configure(feathers.authentication());
 var page = document.getElementsByTagName('page')[0].innerText;
 var note = document.getElementById('note');
 var togg = false;
 var cardnum = '';
 var timer, count = '';
+var memory = '';
+const requestsService = client.service('requests');
 
 
 // console.log(page)
@@ -157,10 +161,10 @@ var showProgress = function(element, type){
     }
     
 }
-// login()
+login()
 
 /************************************************/
-/************* Request table contents ***********/
+/************* Temporary Datasets ***********/
 /************************************************/
 
 var requests = [
@@ -239,7 +243,18 @@ var requests = [
   
   
 
+];
+
+var products = [
+  {}
 ]
+
+
+
+
+/************************************************/
+/************* Temporary Datasets end ***********/
+/************************************************/
 
 
 var render = function(page, component, data, parent){
@@ -252,9 +267,9 @@ var render = function(page, component, data, parent){
           var index = 0;
           html = '';
           for(var t = 0; t < data.length; t++){
-            html += `<tr class="accordion" onclick="toge(this, `;
-            html += t;
-            html += `)"><td>`;
+            html += `<tr class="accordion" role="row" onclick="toge(this, '`;
+            html += data[t]._id;
+            html += `')"><td class="sorting_1 dtr-control">`;
             html += data[t].name;
             html += `<div class="card-body opt"><h5>`
             html += data[t]['shrt-name'];
@@ -264,14 +279,55 @@ var render = function(page, component, data, parent){
             html += data[t].time;
             html += `</p></span></div></td><td class="max">`
             html += data[t].company;
-            
             html += `<div class="card-body opt"><div class="row"><div class="col-md-8"><h5>`;
             html += data[t].subject;
             html += `</h5><p>`;
             html += data[t].content;
-            html += `</p><center><div class="mb-3"><a href="javascript:void(0);" class="btn gp-green replynow-btn waves-effect waves-light" data-bs-toggle="modal" onclick="replynw(`;
-            html += t;
-            html += `)" data-bs-target=".bs-example-modal-center">Reply Now</a></div></center></div><div class="col-md-4"><div class="card card-body"><img src="`
+            html += `</p><center><div class="mb-3"><a href="javascript:void(0);" class="btn gp-green replynow-btn waves-effect waves-light" data-bs-toggle="modal" onclick="replynw('`;
+            html += data[t]._id;
+            html += `')" data-bs-target=".bs-example-modal-center">Reply Now</a></div></center></div><div class="col-md-4"><div class="card card-body"><img src="`
+            html += data[t].image;
+            html += `" alt="" srcset=""></div></div></div></div></td><td class="min">`
+            html += data[t].phone;
+            html += `<div class="card-body opt"></div></td><td class="min">`;
+            html += data[t].email;
+            html += `<div class="card-body opt"></div></td></tr>`
+          }
+          parent.innerHTML = html;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+
+      break;
+    }
+    case 'product': {
+      switch(component){
+        case 'product-table': {
+          var index = 0;
+          html = '';
+          for(var t = 0; t < data.length; t++){
+            html += `<tr class="accordion" role="row" onclick="toge(this, '`;
+            html += data[t]._id;
+            html += `')"><td class="sorting_1 dtr-control">`;
+            html += data[t].name;
+            html += `<div class="card-body opt"><h5>`
+            html += data[t]['shrt-name'];
+            html += `</h5><span class="req-datetime"><p>`
+            html += data[t].date;
+            html += `</p><p> `
+            html += data[t].time;
+            html += `</p></span></div></td><td class="max">`
+            html += data[t].company;
+            html += `<div class="card-body opt"><div class="row"><div class="col-md-8"><h5>`;
+            html += data[t].subject;
+            html += `</h5><p>`;
+            html += data[t].content;
+            html += `</p><center><div class="mb-3"><a href="javascript:void(0);" class="btn gp-green replynow-btn waves-effect waves-light" data-bs-toggle="modal" onclick="replynw('`;
+            html += data[t]._id;
+            html += `')" data-bs-target=".bs-example-modal-center">Reply Now</a></div></center></div><div class="col-md-4"><div class="card card-body"><img src="`
             html += data[t].image;
             html += `" alt="" srcset=""></div></div></div></div></td><td class="min">`
             html += data[t].phone;
@@ -297,14 +353,42 @@ var render = function(page, component, data, parent){
 
 switch(page){
     case 'requests': {
+      var reqflx = document.getElementById('reqflx');
+      for(var l = 0; l < reqflx.children.length; l++){
+        reqflx.children[l].addEventListener('click', function(ev, th = this ){
+          for(var k = 0; k < reqflx.children.length; k++){
+            reqflx.children[k].classList.value = [''];
+          }
+          th.classList.value = ['req-active'];
+        });
+      };
+
       ClassicEditor
         .create( document.querySelector( '#classic-editor' ) )
         .catch( error => {
             console.error( error );
         } );
-      render('request', 'request-table', requests, 'request_table');
+
+      requestsService.find().then( req => {
+        memory = req;
+        render('request', 'request-table', req, 'request_table');
+        $("#datatable").DataTable();
+        
+        // console.log(memory[0]._id);
+      });
+
+      // render('request', 'request-table', requests, 'request_table');
+
+
+      
       break;
         
+    }
+
+    case 'products': {
+      productPageInit();
+
+      break;
     }
     
     
@@ -423,31 +507,128 @@ function togglerow(row, state){
   }
 }
 
-var reqflx = document.getElementById('reqflx');
-for(var l = 0; l < reqflx.children.length; l++){
-  reqflx.children[l].addEventListener('click', function(ev, th = this ){
-    for(var k = 0; k < reqflx.children.length; k++){
-      reqflx.children[k].classList.value = [''];
-    }
-    th.classList.value = ['req-active'];
-  });
-}
 
-function replynw(ele){
+
+async function replynw(ele){
   var replyto = document.getElementsByClassName('replyto');
   var replyfrom = document.getElementById('replyfrom');
   var reply_sub = document.getElementById('reply_sub');
-  
+  console.log(ele);
+  var request = await requestsService.get(ele);
   for(var h = 0; h < replyto.length; h++){
-    replyto[h].innerText = requests[ele].email;
+    console.log(request.email);
+    replyto[h].innerText = request.email;
   }
   replyfrom.innerText = 'olomije.ezekiel@greenpegltd.com';
   reply_sub.value = 'Request Subject';
 }
 
 
+function validateFileType(){
+  var fileName = document.getElementById("fileName").value;
+  var idxDot = fileName.lastIndexOf(".") + 1;
+  var extFile = fileName.substr(idxDot, fileName.length).toLowerCase();
+  if (extFile=="jpg" || extFile=="jpeg" || extFile=="png"){
+      //TO DO
+  }else{
+      alert("Only jpg/jpeg and png files are allowed!");
+  }   
+}
+
+
+var // Define maximum number of files.
+      max_file_number = 3,
+      // Define your form id or class or just tag.
+      $form = $('#uplimg'), 
+      // Define your upload field class or id or tag.
+      $file_upload = $('#fileName', $form), 
+      // Define your submit class or id or tag.
+      $button = $('.complete-btn', $form); 
+
+  // Disable submit button on page ready.
+  $button.prop('disabled', 'disabled');
+
+  $file_upload.on('change', function () {
+    var number_of_images = $(this)[0].files.length;
+    if (number_of_images > max_file_number) {
+      alert(`You can upload maximum ${max_file_number} files.`);
+      $(this).val('');
+      $button.prop('disabled', 'disabled');
+    } else {
+      $button.prop('disabled', false);
+    }
+  });
 
 
 
 
+function productPageInit(){
+  console.log(page);
+  var crd = [];
+  var lst = [ '.product-table', '.noprod', '.addnewprod', '.editprod'];
+  var cnt = 0;
+  lst.forEach( card => {
+    if(cnt < lst.length){
+      console.log(cnt);
+      if(card.split('.')[0] == ''){
+        var crde = document.getElementsByClassName(card.split('.')[1]);
+        console.log(crde[0]);
+        crd.push(crde[0]);
+        crde[0].style.display = 'none';
+      }
+      
+      else{
+        console.log(card);
+      }
+    }
+    
+    cnt++
+    
+    
+    
+  })
+  if(products.length < 1){
+      crd[1].style.display = 'block';
+  }
+  else{
+    crd[0].style.display = 'block';
+  }
+  $("#datatable").DataTable();
+  
+}
 
+function toggleScreen(screen){
+  var crd = [];
+  var lst = [ '.product-table', '.noprod', '.addnewprod', '.editprod'];
+  var cnt = 0;
+  lst.forEach( card => {
+    if(cnt < lst.length){
+      console.log(cnt);
+      if(card.split('.')[0] == ''){
+        var crde = document.getElementsByClassName(card.split('.')[1]);
+        console.log(crde[0]);
+        crd.push(crde[0]);
+        crde[0].style.display = 'none';
+      }
+      
+      else{
+        console.log(card);
+      }
+    }
+    cnt++
+  })
+  if(lst.includes(screen)){
+    var scrn = document.getElementsByClassName(screen.split('.')[1]);
+    scrn[0].style.display = 'block';
+  }
+  else{
+    crd[1].style.display = 'block';
+  }
+}
+
+
+
+function dtInit(){
+  $("#datatable").DataTable()
+  // $("#datatable").DataTable(),$("#datatable-buttons").DataTable({lengthChange:!1,buttons:["copy","excel","pdf","colvis"]}).buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)"),$(".dataTables_length select").addClass("form-select form-select-sm");
+}
