8466bb7278d0a110c63072de2f6a9c06c97ce7f4
emmytheo <chidi.mgbara@gmail.com>
2022-03-15 16:08:56 +0100

    update

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.use(compress())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(favicon(path.join(app.get('public'), 'favicon.ico')))
+// app.use(favicon(path.join(app.get('public'), 'favicon.ico')))
 // Host the public folder
 app.use('/', express.static(app.get('public')))
 
